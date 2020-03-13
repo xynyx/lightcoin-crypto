a303ae0fdf2f58ef00759e7df1108d9305031dbc
Matt Taylor <matt.taylr22@gmail.com>
2020-03-13 23:53:28 +0000

    finished LightCoin

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-let balance = 500.00;
+class Transaction {
 
-class Withdrawal {
-
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.canWithdrawal()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -(this.amount);
   }
 
-}
+  canWithdrawal() {
+    return (this.account.balance - this.amount >= 0);
+  }
+};
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+
+  canWithdrawal() {
+    return true;
+  }
+};
+
+class Account {
+  constructor() {
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (const changed of this.transactions) {
+      balance += changed.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+};
 
 
 
@@ -18,12 +58,31 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("snow-patrol");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// Passing the object the information when you need it (passing myAccount) is good design
+// Withdrawal and Deposit are not dependent on any surrounding data in their global or outer scope
+// Now transacations are not tied to any single account
+// "DEPENDENCY INJECTION" -> passing object the things it needs rather than having object access them itself
 
-console.log('Balance:', balance);
+console.log('Starting Account Balance: ', myAccount.balance);
+
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
+
+// console.log('Ending Account Balance: ', myAccount.balance);
+// console.log("Lookings like I'm broke again");
+
+// console.log('Account Transaction History: ', myAccount.transactions);
+
